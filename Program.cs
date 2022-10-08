8335d599d1fa7e2a811590a589706cb8cea5ebb6
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-10-08 14:20:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace array_practice;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов массива:\t");
+         
+        int elementscount = int.Parse(Console.ReadLine());
+
+        int[] array = new int[elementscount];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"\nВведите эелемент массива под индексом {i}:\t ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("\nВывод элементoв массива ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}

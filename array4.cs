80c6543e34bc98374de583b41e90aad45bd4b8c1
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-10-13 21:21:22 +0300

    Add files via upload

---
+++ array4.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+namespace array4;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 4, 35, 12, 67, 37, 41 };
+
+        int minValue = array[0];
+        
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minValue)
+            { 
+                minValue = array[i];    
+            }
+        }
+        Console.WriteLine(minValue);
+
+
+        Console.ReadLine();
+    }
+}

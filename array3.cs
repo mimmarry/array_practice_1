bf467c416569be1527302aa164d85ec572546916
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-10-13 21:15:30 +0300

    Add files via upload

---
+++ array3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace array3;
+class Programm
+{
+    static void Main(string[] args)
+    {
+
+
+        int[] array = { 4, 35, 12, 67, 37, 41 };
+        
+        int result = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                result += array[i];
+            }
+        }
+        Console.WriteLine("Сумма четных чисел массива равна " + result);
+
+
+        Console.ReadLine();
+    }
+}

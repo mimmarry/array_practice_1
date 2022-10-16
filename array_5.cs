e0f3da5a85acaebd22cf295d7355802497b15867
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-10-16 22:37:39 +0300

    Add files via upload

---
+++ array_5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace array_5;
+class Programm
+{
+    static void Main()
+    {
+
+        Console.Write("Введите количество элементов в вашем массиве :");
+        int elements = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[elements];  
+       for(int i = 0; i < array.Length;i++)
+        {
+            Console.WriteLine($"\nВведите элемент массива под индексом {i}: \t");
+            array[i] = Convert.ToInt32(Console.ReadLine()); 
+        }
+       Console.WriteLine("Наименьшее значение элемента из массива - " + array.Min());
+
+        Console.ReadKey();
+    }
+        
+}
